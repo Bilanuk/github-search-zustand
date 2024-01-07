7ec6063b89326791123b3c1bc62018aa17b27983
Roman <bilanuk.bill@gmail.com>
2024-01-08 00:55:35 +0200

    reformat code

--- src/store/repository/repositorySlice.ts
+++ src/store/repository/repositorySlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
 import { Repository } from '../../__generated__/graphql'
 
